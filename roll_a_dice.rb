2903de832f06e5044fa229b1c9760b90329719a8
Tenseisan <edotensei19@gmail.com>
2018-02-22 17:50:13 +0700

    add show dice method

--- roll_a_dice.rb
+++ roll_a_dice.rb
@@ -1,7 +1,17 @@
-# encoding: utf-8
-#
-# Программа, выбрасывающая значение от 1 до 6 — одну игральную кость
-#
-# (с) goodprogrammer.ru
 
-puts rand(6) + 1
+def show_rolling_dice
+  100.times do
+    print "#{rand(6) + 1}\r"
+    sleep 0.01
+  end
+end
+
+
+puts "Сколько раз кинуть кубик? :"
+
+number = gets.to_i
+
+number.times do
+  show_rolling_dice
+  puts rand(6) + 1
+end
